7961829f843077c0b43ffd3774ab91ff23cd9b95
Anokhi Soni <pokemonkagodarceus@gmail.com>
2026-06-29 14:15:21 +0530

    Replaced duplicates from support section

--- frontend/src/landingPage/support/CreateTicket.js
+++ frontend/src/landingPage/support/CreateTicket.js
@@ -6,11 +6,11 @@ function CreateTicket() {
         <div className="mainContainer">
             <div className="accountRelated">
                 <CreateTicketSubCompo id={"plate1"} icon={<i className="fa-solid fa-circle-plus"></i>} heading={"Account Opening"} linkList={["Resident individual", "Minor", "Non Resident Indian (NRI)", "Company, Partnership, HUF and LLP", "Glossary"]}  />
-                <CreateTicketSubCompo id={"plate2"} icon={<i className="fa-solid fa-circle-plus"></i>} heading={"Account Opening"} linkList={["Resident individual", "Minor", "Non Resident Indian (NRI)", "Company, Partnership, HUF and LLP", "Glossary"]}  />
-                <CreateTicketSubCompo id={"plate3"} icon={<i className="fa-solid fa-circle-plus"></i>} heading={"Account Opening"} linkList={["Resident individual", "Minor", "Non Resident Indian (NRI)", "Company, Partnership, HUF and LLP", "Glossary"]}  />
-                <CreateTicketSubCompo id={"plate4"} icon={<i className="fa-solid fa-circle-plus"></i>} heading={"Account Opening"} linkList={["Resident individual", "Minor", "Non Resident Indian (NRI)", "Company, Partnership, HUF and LLP", "Glossary"]}  />
-                <CreateTicketSubCompo id={"plate3"} icon={<i className="fa-solid fa-circle-plus"></i>} heading={"Account Opening"} linkList={["Resident individual", "Minor", "Non Resident Indian (NRI)", "Company, Partnership, HUF and LLP", "Glossary"]}  />
-                <CreateTicketSubCompo id={"plate4"} icon={<i className="fa-solid fa-circle-plus"></i>} heading={"Account Opening"} linkList={["Resident individual", "Minor", "Non Resident Indian (NRI)", "Company, Partnership, HUF and LLP", "Glossary"]}  />
+                <CreateTicketSubCompo id={"plate2"} icon={<i className="fa-solid fa-circle-plus"></i>} heading={"Your Zerodha Account"} linkList={ ["Your Profile", "Account modification", "Client Master Report (CMR) and Depository Participant (DP)", "Nomination", "Transfer and conversion of securities"]}  />
+                <CreateTicketSubCompo id={"plate3"} icon={<i className="fa-solid fa-circle-plus"></i>} heading={"Kite"} linkList={["IPO", "Trading FAQs", "Margin Trading Facility (MTF) and Margins", "Charts and orders", "Alerts and Nudges", "General"]}  />
+                <CreateTicketSubCompo id={"plate4"} icon={<i className="fa-solid fa-circle-plus"></i>} heading={"Funds"} linkList={["Add money", "Withdraw money", "Add bank accounts", "eMandates"]}  />
+                <CreateTicketSubCompo id={"plate3"} icon={<i className="fa-solid fa-circle-plus"></i>} heading={"Console"} linkList={["Portfolio", "Corporate actions", "Funds statement", "Reports", "Profile", "Segments"]}  />
+                <CreateTicketSubCompo id={"plate4"} icon={<i className="fa-solid fa-circle-plus"></i>} heading={"Coin"} linkList={["Mutual funds", "National Pension Scheme (NPS)", "Fixed Deposit (FD)", "Features on Coin", "Payments and Orders", "General"]}  />
             </div>
             <div className="links">
                 <ul>
